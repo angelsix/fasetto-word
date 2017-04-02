31458f0f189ae9d6b8059bc3a2f036498cf75600
Unknown <contact@angelsix.com>
2017-04-02 06:26:58 +0100

    Chat List ItemsControl

---
+++ Source/Fasetto.Word/Controls/Chat/ChatListControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Fasetto.Word
+{
+    /// <summary>
+    /// Interaction logic for ChatListControl.xaml
+    /// </summary>
+    public partial class ChatListControl : UserControl
+    {
+        public ChatListControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Source/Fasetto.Word/ViewModel/Chat/ChatListItemViewModel.cs
@@ -0,0 +1,39 @@
+﻿namespace Fasetto.Word
+{
+    /// <summary>
+    /// A view model for each chat list item in the overview chat list
+    /// </summary>
+    public class ChatListItemViewModel : BaseViewModel
+    {
+        /// <summary>
+        /// The display name of this chat list
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The latest message from this chat
+        /// </summary>
+        public string Message { get; set; }
+
+        /// <summary>
+        /// The initials to show for the profile picture background
+        /// </summary>
+        public string Initials { get; set; }
+
+        /// <summary>
+        /// The RGB values (in hex) for the background color of the profile picture
+        /// For example FF00FF for Red and Blue mixed
+        /// </summary>
+        public string ProfilePictureRGB { get; set; } 
+
+        /// <summary>
+        /// True if there are unread messages in this chat 
+        /// </summary>
+        public bool NewContentAvailable { get; set; }
+
+        /// <summary>
+        /// True if this item is currently selected
+        /// </summary>
+        public bool IsSelected { get; set; }
+    }
+}
