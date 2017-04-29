4bd09fbc07b2823e92bd002ecd57f78d7573a619
Unknown <contact@angelsix.com>
2017-04-29 10:02:52 +0100

    Start making chat bubbles

---
+++ Source/Fasetto.Word/Controls/Chat/ChatMessage/ChatMessageListItemControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Fasetto.Word
+{
+    /// <summary>
+    /// Interaction logic for ChatMessageListItemControl.xaml
+    /// </summary>
+    public partial class ChatMessageListItemControl : UserControl
+    {
+        public ChatMessageListItemControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
