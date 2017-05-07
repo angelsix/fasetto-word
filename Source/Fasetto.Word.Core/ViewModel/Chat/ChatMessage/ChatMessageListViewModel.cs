43d8a036d79947f3432bfd500a03a2faebb1fd68
Unknown <contact@angelsix.com>
2017-05-07 17:05:09 +0100

    Send message chat bubble styling

---
+++ Source/Fasetto.Word.Core/ViewModel/Chat/ChatMessage/ChatMessageListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Fasetto.Word.Core
+{
+    /// <summary>
+    /// A view model for a chat message thread list
+    /// </summary>
+    public class ChatMessageListViewModel : BaseViewModel
+    {
+        /// <summary>
+        /// The chat thread items for the list
+        /// </summary>
+        public List<ChatMessageListItemViewModel> Items { get; set; }
+    }
+}
---
+++ Source/Fasetto.Word/ValueConverters/SentByMeToBackgroundConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+
+namespace Fasetto.Word
+{
+    /// <summary>
+    /// A converter that takes in a boolean if a message was sent by me, and returns the
+    /// correct background color
+    /// </summary>
+    public class SentByMeToBackgroundConverter : BaseValueConverter<SentByMeToBackgroundConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value ? Application.Current.FindResource("WordVeryLightBlueBrush") : Application.Current.FindResource("ForegroundLightBrush");
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
