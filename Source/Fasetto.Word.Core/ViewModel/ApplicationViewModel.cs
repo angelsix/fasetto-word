5902d73f554c44ae41002e6cb2688839d60f60ea
Unknown <contact@angelsix.com>
2017-05-20 10:14:46 +0100

    Start of chat message input control

--- Source/Fasetto.Word.Core/ViewModel/ApplicationViewModel.cs
+++ Source/Fasetto.Word.Core/ViewModel/ApplicationViewModel.cs
@@ -14,12 +14,12 @@ namespace Fasetto.Word.Core
         /// <summary>
         /// The current page of the application
         /// </summary>
-        public ApplicationPage CurrentPage { get; private set; } = ApplicationPage.Login;
+        public ApplicationPage CurrentPage { get; private set; } = ApplicationPage.Chat;
 
         /// <summary>
         /// True if the side menu should be shown
         /// </summary>
-        public bool SideMenuVisible { get; set; } = false;
+        public bool SideMenuVisible { get; set; } = true;
 
         /// <summary>
         /// Navigates to the specified page
