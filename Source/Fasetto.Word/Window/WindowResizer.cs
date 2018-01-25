42ad5dab62f3373ad28129d37ba77d0b77d0b35c
Unknown <contact@angelsix.com>
2018-01-25 18:14:31 +0000

    Cleanup some standards

--- Source/Fasetto.Word/Window/WindowResizer.cs
+++ Source/Fasetto.Word/Window/WindowResizer.cs
@@ -326,7 +326,7 @@ namespace Fasetto.Word
         private void WmGetMinMaxInfo(System.IntPtr hwnd, System.IntPtr lParam)
         {
             // Get the point position to determine what screen we are on
-            GetCursorPos(out POINT lMousePosition);
+            GetCursorPos(out var lMousePosition);
 
             // Now get the current screen
             var lCurrentScreen = mBeingMoved ?
@@ -396,7 +396,7 @@ namespace Fasetto.Word
                 // NOTE: rcMonitor is the monitor size
                 //       rcWork is the available screen size (so the area inside the task bar start menu for example)
 
-                // Size size limits (used by Windows when maximized)
+                // Size limits (used by Windows when maximized)
                 // relative to 0,0 being the current screens top-left corner
 
                 // Set to primary monitor size
@@ -436,7 +436,7 @@ namespace Fasetto.Word
         public Point GetCursorPosition()
         {
             // Get mouse position
-            GetCursorPos(out POINT lMousePosition);
+            GetCursorPos(out var lMousePosition);
 
             // Apply DPI scaling
             return new Point(lMousePosition.X / mMonitorDpi.Value.DpiScaleX, lMousePosition.Y / mMonitorDpi.Value.DpiScaleY);
@@ -445,7 +445,7 @@ namespace Fasetto.Word
 
     #region DLL Helper Structures
 
-    enum MonitorOptions : uint
+    public enum MonitorOptions : uint
     {
         MONITOR_DEFAULTTONULL = 0x00000000,
         MONITOR_DEFAULTTOPRIMARY = 0x00000001,
